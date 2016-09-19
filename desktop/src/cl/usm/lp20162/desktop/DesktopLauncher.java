c6fe5ffeb1f9779b8d2f46a4742ba82db0ddd5e4
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-09-19 18:02:13 -0300

    first

---
+++ desktop/src/cl/usm/lp20162/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cl.usm.lp20162.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cl.usm.lp20162.TestTarea2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestTarea2(), config);
+	}
+}
